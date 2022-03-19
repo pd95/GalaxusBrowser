5e4eb499e73d59e00214f44e7de00db0c158f764
Philipp <pd95@users.noreply.github.com>
2022-03-19 12:44:48 +0100

    Make sure swipe gestures are accepted by WebView

--- GalaxusBrowser/WebView.swift
+++ GalaxusBrowser/WebView.swift
@@ -13,7 +13,9 @@ struct WebView : UIViewRepresentable {
     let request: URLRequest
 
     func makeUIView(context: Context) -> WKWebView  {
-        return WKWebView()
+        let webView = WKWebView()
+        webView.allowsBackForwardNavigationGestures = true
+        return webView
     }
 
     func updateUIView(_ uiView: WKWebView, context: Context) {
