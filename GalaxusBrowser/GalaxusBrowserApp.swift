d55782c3666826f7f5331095ac89850035aa3bcc
Philipp <pd95@users.noreply.github.com>
2022-03-19 10:10:37 +0100

    Initial Commit

---
+++ GalaxusBrowser/GalaxusBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalaxusBrowserApp.swift
+//  GalaxusBrowser
+//
+//  Created by Philipp on 19.03.22.
+//
+
+import SwiftUI
+
+@main
+struct GalaxusBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
